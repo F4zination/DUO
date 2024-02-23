c10afb29cc149087ea50ca0c3a5098311341da14
oriventi <adrianus3010@gmail.com>
2024-02-23 22:54:32 +0100

    just sending one message

--- duo_backend/api/server_routes.go
+++ duo_backend/api/server_routes.go
@@ -3,25 +3,19 @@ package api
 import (
 	"context"
 	"log"
-	"time"
 
 	"github.com/duo/pb"
 )
 
 func (server *Server) Connect(empty *pb.Empty, stream pb.MessagingService_ConnectServer) error {
 	log.Printf("Client connected")
-	go func() {
-		i := 0
-		for {
-			err := stream.Send(&pb.Request{Name: string(rune(i))})
-			if err != nil {
-				return
-			}
-			log.Printf("Sent message %d", i)
-			i++
-			time.Sleep(1 * time.Second)
-		}
-	}()
+	i := 0
+	err := stream.Send(&pb.Request{Name: string(rune(i))})
+	if err != nil {
+		return err
+	}
+	log.Printf("Sent message %d", i)
+	i++
 	return nil
 }
 
