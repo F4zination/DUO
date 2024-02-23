5aa17bdd5e1b8677f11da90820827750e45ff8e6
oriventi <adrianus3010@gmail.com>
2024-02-23 23:01:02 +0100

    sending a num every sec in connect

--- duo_backend/api/server_routes.go
+++ duo_backend/api/server_routes.go
@@ -2,21 +2,37 @@ package api
 
 import (
 	"context"
+	"fmt"
 	"log"
+	"time"
 
 	"github.com/duo/pb"
 )
 
 func (server *Server) Connect(empty *pb.Empty, stream pb.MessagingService_ConnectServer) error {
 	log.Printf("Client connected")
-	i := 0
-	err := stream.Send(&pb.Request{Name: "Hello"})
-	if err != nil {
-		return err
+	reqChan := make(chan *pb.Request)
+	go func() {
+		i := 0
+		for {
+			log.Printf("Sending request %d", i)
+			reqChan <- &pb.Request{Name: fmt.Sprintf("Num: %d", i)}
+			i++
+			time.Sleep(1 * time.Second)
+		}
+	}()
+	for {
+		select {
+		case req := <-reqChan:
+			err := stream.Send(req)
+			if err != nil {
+				return err
+			}
+		case <-stream.Context().Done():
+			log.Printf("Client disconnected")
+			return nil
+		}
 	}
-	log.Printf("Sent message %d", i)
-	i++
-	return nil
 }
 
 func (server *Server) Send(context context.Context, request *pb.Request) (*pb.Empty, error) {
