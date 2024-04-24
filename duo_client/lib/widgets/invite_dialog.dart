76ffa22a7be3a5156e6f3de0318f8382990198ac
oriventi <adrianus3010@gmail.com>
2024-04-24 15:55:39 +0200

    added different code sharing methods

--- duo_client/lib/widgets/invite_dialog.dart
+++ duo_client/lib/widgets/invite_dialog.dart
@@ -2,7 +2,9 @@ import 'package:duo_client/pb/friend.pb.dart';
 import 'package:duo_client/provider/friend_provider.dart';
 import 'package:duo_client/utils/constants.dart';
 import 'package:duo_client/widgets/friend_list_tile.dart';
+import 'package:duo_client/widgets/qr_join_dialog.dart';
 import 'package:flutter/material.dart';
+import 'package:share_plus/share_plus.dart';
 
 class InviteDialog extends StatelessWidget {
   InviteDialog({super.key, required this.invideCode});
@@ -42,24 +44,48 @@ class InviteDialog extends StatelessWidget {
                   );
                 }).toList(),
               )),
-              const SizedBox(height: 20),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Text('Join via Code:  ${invideCode.toString()}',
-                      style:
-                          const TextStyle(color: Colors.white, fontSize: 16)),
-                  const SizedBox(width: 20),
-                  ElevatedButton(
-                    onPressed: () {
-                      Navigator.of(context).pop();
-                    },
-                    child: const Text('Cancel',
-                        style: TextStyle(color: Colors.white)),
-                  ),
-                ],
+              const SizedBox(height: Constants.defaultPadding),
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                    horizontal: Constants.defaultPadding * 2),
+                child: Row(
+                  children: [
+                    Text(
+                      'Join via Code:  ${invideCode.toString()}',
+                      style: const TextStyle(color: Colors.white, fontSize: 16),
+                    ),
+                    const Spacer(),
+                    IconButton(
+                      onPressed: () => showDialog(
+                          context: context,
+                          builder: (context) => QrJoinDialog(
+                                id: invideCode.toString(),
+                              )),
+                      icon: const Icon(
+                        Icons.qr_code_2_rounded,
+                        color: Colors.white54,
+                      ),
+                    ),
+                    const SizedBox(width: Constants.defaultPadding / 2),
+                    IconButton(
+                      icon: const Icon(Icons.share, color: Colors.white54),
+                      onPressed: () async {
+                        await Share.share(
+                            'Join me on Duo using this code: *$invideCode*');
+                      },
+                    )
+                  ],
+                ),
               ),
-              const SizedBox(height: 20),
+              const SizedBox(height: Constants.defaultPadding),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                child:
+                    const Text('Cancel', style: TextStyle(color: Colors.white)),
+              ),
+              const SizedBox(height: Constants.defaultPadding),
             ],
           )),
     );
---
+++ duo_client/lib/widgets/qr_join_dialog.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:qr_flutter/qr_flutter.dart';
+
+class QrJoinDialog extends StatelessWidget {
+  final String id;
+  const QrJoinDialog({required this.id, super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Dialog(
+      child: Container(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            SizedBox(
+              width: MediaQuery.of(context).size.width * 0.5,
+              child: const Text(
+                  'Scan this QR code with the DUO app to join the group:',
+                  style: TextStyle(fontSize: 18, color: Colors.white70),
+                  textAlign: TextAlign.center),
+            ),
+            const SizedBox(height: 10),
+            QrImageView(
+              data: 'id:$id',
+              version: QrVersions.auto,
+              foregroundColor: Colors.white54,
+              size: 200,
+            ),
+            const SizedBox(height: 10),
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: const Text('OK'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
